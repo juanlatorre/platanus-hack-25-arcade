82b90966f62dd17c01f64954f0823e287b4e34ca
Juan Latorre <juanlatorreharcha@gmail.com>
2025-10-29 19:23:40 -0300

    add birds

--- game.js
+++ game.js
@@ -25,10 +25,57 @@ function preload() {
   // No assets to preload
 }
 
+function drawBird(graphics, x, y, size, color, isZarapito = false) {
+  // Body
+  graphics.fillStyle(color, 1);
+  graphics.fillCircle(x, y, size * 0.4);
+
+  // Head
+  graphics.fillCircle(x + size * 0.3, y - size * 0.2, size * 0.25);
+
+  // Beak
+  graphics.fillStyle(0xffaa00, 1);
+  graphics.fillTriangle(
+    x + size * 0.45, y - size * 0.15,
+    x + size * 0.6, y - size * 0.1,
+    x + size * 0.45, y - size * 0.05
+  );
+
+  // Wings
+  graphics.fillStyle(color, 0.8);
+  graphics.fillEllipse(x - size * 0.1, y, size * 0.4, size * 0.3);
+  graphics.fillEllipse(x + size * 0.1, y, size * 0.4, size * 0.3);
+
+  // Eye
+  graphics.fillStyle(0xffffff, 1);
+  graphics.fillCircle(x + size * 0.35, y - size * 0.25, size * 0.05);
+  graphics.fillStyle(0x000000, 1);
+  graphics.fillCircle(x + size * 0.35, y - size * 0.25, size * 0.02);
+
+  if (isZarapito) {
+    graphics.fillStyle(0xff0000, 1);
+    graphics.fillCircle(x + size * 0.3, y - size * 0.3, size * 0.03);
+  }
+}
+
 function create() {
-  // To be implemented
+  this.graphics = this.add.graphics();
+
+  // Menu text
+  this.add.text(400, 150, "Zarapito's Symphony Skirmish", { fontSize: '32px', color: '#00ffff' }).setOrigin(0.5);
+  this.add.text(400, 350, 'Press SPACE to Start', { fontSize: '20px', color: '#00ff00' }).setOrigin(0.5);
+}
+
+function drawMenu(scene) {
+  scene.graphics.clear();
+
+  // Decorative birds
+  drawBird(scene.graphics, 200, 400, 25, 0x00ffff);
+  drawBird(scene.graphics, 600, 400, 25, 0xff00ff, true);
 }
 
 function update() {
-  // To be implemented
+  if (gameState === 'menu') {
+    drawMenu(this);
+  }
 }
